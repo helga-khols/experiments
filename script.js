ff15f087ba1004f6aa482016f43fbc7e7f34a3d7
helga-khols <helga.khols@gmail.com>
2022-09-05 22:33:58 +0300

    w

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
-const toggle = document.getElementById('toggle');
-const sidebar = document.getElementById('sidebar');
-const closemenu = document.getElementById('closemenu');
-
-document.onclick = function (e) {
-  if (e.target.id !== 'sidebar' && e.target.id !== 'toggle') {
-    toggle.classList.remove('active');
-    sidebar.classList.remove('active');
-  }
-};
-
-toggle.onclick = function () {
-  toggle.classList.toggle('active');
-  sidebar.classList.toggle('active');
-};
-closemenu.onclick = function () {
-  toggle.classList.toggle('active');
-  sidebar.classList.toggle('active');
-};
+const menuItemsByClass = document.querySelectorAll(".menu-item");
+console.log(menuItemsByClass);
